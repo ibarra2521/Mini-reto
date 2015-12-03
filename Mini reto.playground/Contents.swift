8def37e6aac0e1181a44a6294fa72c0bd31a55cb
ibarra2521 <ibarra2521@gmail.com>
2015-12-03 17:48:49 -0500

    Mini reto terminado v1

--- Mini reto.playground/Contents.swift
+++ Mini reto.playground/Contents.swift
@@ -2,5 +2,15 @@
 
 import UIKit
 
-var str = "Hello, playground in GitHub"
-var string = "modificación"
+for index in 0...100 {
+    // First version
+    if index >= 30 && index <= 40 {
+        print("\(index) Viva Swift!!!")
+    }else if index % 5 == 0{
+        print("\(index) Bingo!!!")
+    }else if index % 2 == 0 {
+        print("\(index) Par")
+    }else if index % 2 == 1 {
+        print("\(index) Impar")
+    }
+}
